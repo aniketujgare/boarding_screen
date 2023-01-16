fac38fd1e11eccd07203fb1edf0b265cb3fa5e54
Aniket Subhash Ujgare <aniketujgare@gmail.com>
2023-01-16 12:48:39 +0530

    before stack

--- lib/main.dart
+++ lib/main.dart
@@ -28,57 +28,93 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: PageView(
-                  controller: PageController(initialPage: 0),
-                  scrollDirection: Axis.horizontal,
-                  pageSnapping: true,
-                  children: const [
-                    BoardingPage1(),
-                    BoardingPage2(),
-                    BoardingPage3(),
-                    BoardingPage4(),
-                    BoardingPage5(),
-                    BoardingPage6(),
-                  ],
-                ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _page = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 10,
+              child: PageView(
+                controller: PageController(initialPage: 0),
+                scrollDirection: Axis.horizontal,
+                onPageChanged: (value) {
+                  _page = value;
+                  setState(() {});
+                },
+                pageSnapping: true,
+                children: const [
+                  BoardingPage1(),
+                  BoardingPage2(),
+                  BoardingPage3(),
+                  BoardingPage4(),
+                  BoardingPage5(),
+                  BoardingPage6(),
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    DottedBorder(
-                      borderType: BorderType.Circle,
-                      dashPattern: const [5],
-                      color: kPrimaryColor,
-                      child: CircleAvatar(
-                        radius: 9,
-                        backgroundColor: kPrimaryColor,
-                      ),
-                    ),
-                    for (int i = 0; i < 5; i++) ...[
-                      DottedBorder(
-                        borderType: BorderType.Circle,
-                        dashPattern: const [5],
-                        color: Colors.transparent,
-                        child: CircleAvatar(
-                          radius: 7,
-                          backgroundColor: kSecondaryColor,
+            ),
+            const Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints.expand(height: 22),
+                      child: Center(
+                        child: ListView.separated(
+                          itemCount: 6,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          physics: const NeverScrollableScrollPhysics(),
+                          separatorBuilder: (context, index) => const SizedBox(
+                            width: 5,
+                          ),
+                          itemBuilder: (context, position) {
+                            int i = position;
+                            return DottedBorder(
+                              borderType: BorderType.Circle,
+                              dashPattern: const [5],
+                              color: i == _page
+                                  ? kPrimaryColor
+                                  : Colors.transparent,
+                              child: CircleAvatar(
+                                radius: i == _page ? 9 : 7,
+                                backgroundColor: i == _page
+                                    ? kPrimaryColor
+                                    : kSecondaryColor,
+                              ),
+                            );
+                          },
                         ),
                       ),
-                      const SizedBox(width: 2),
-                    ]
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.12,
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/boarding_pages/boarding_page5.dart
+++ lib/screens/boarding_pages/boarding_page5.dart
@@ -1,5 +1,4 @@
 import 'package:boarding_screen/constants.dart';
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -33,8 +32,6 @@ class VerticalView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double inactiveSize = 7;
-    double activeSize = 9;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -42,30 +39,10 @@ class VerticalView extends StatelessWidget {
           height: height * 0.1348,
         ),
         Expanded(
-          flex: 4,
+          flex: 8,
           child: Image.asset(
             image,
             fit: BoxFit.cover,
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-            child: FittedBox(
-              alignment: Alignment.topCenter,
-              fit: BoxFit.contain,
-              child: Text(
-                caption,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.dosis(
-                  height: 1.6,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: kTextColor,
-                ),
-              ),
-            ),
           ),
         ),
         const Expanded(
@@ -73,28 +50,21 @@ class VerticalView extends StatelessWidget {
           child: SizedBox(),
         ),
         Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (int i = 1; i <= 6; i++) ...[
-                DottedBorder(
-                  borderType: BorderType.Circle,
-                  dashPattern: const [5],
-                  color: i == page ? kPrimaryColor : Colors.transparent,
-                  child: CircleAvatar(
-                    radius: i == page ? activeSize : inactiveSize,
-                    backgroundColor:
-                        i == page ? kPrimaryColor : kSecondaryColor,
-                  ),
-                ),
-                const SizedBox(width: 2),
-              ]
-            ],
+          flex: 4,
+          child: FittedBox(
+            alignment: Alignment.topCenter,
+            fit: BoxFit.scaleDown,
+            child: Text(
+              caption,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.dosis(
+                height: 1.6,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: kTextColor,
+              ),
+            ),
           ),
-        ),
-        SizedBox(
-          height: height * 0.1348,
         ),
       ],
     );
@@ -106,9 +76,6 @@ class HorizontalView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double inactiveSize = 7;
-    double activeSize = 9;
-
     return Column(
       children: [
         Expanded(
@@ -128,10 +95,9 @@ class HorizontalView extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 25, vertical: 15),
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
                       child: FittedBox(
-                        fit: BoxFit.contain,
+                        fit: BoxFit.scaleDown,
                         child: Text(
                           caption,
                           textAlign: TextAlign.center,
@@ -146,33 +112,6 @@ class HorizontalView extends StatelessWidget {
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              DottedBorder(
-                borderType: BorderType.Circle,
-                dashPattern: const [5],
-                color: kPrimaryColor,
-                child: CircleAvatar(
-                  radius: activeSize,
-                  backgroundColor: kPrimaryColor,
-                ),
-              ),
-              for (int i = 0; i < 5; i++)
-                DottedBorder(
-                  borderType: BorderType.Circle,
-                  dashPattern: const [5],
-                  color: Colors.transparent,
-                  child: CircleAvatar(
-                    radius: inactiveSize,
-                    backgroundColor: kSecondaryColor,
-                  ),
-                ),
             ],
           ),
         ),
